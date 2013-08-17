69b72c341fc6b7aaa1222ea4e3f487a11b5fa3bc
bradmbaker <bradmbak@gmail.com>
2013-08-17 09:11:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,12 @@
-/*
- * Module dependencies
- */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
-var app = express()
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-))
-app.use(express.static(__dirname + '/public'))
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
-
-app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home',
-    alert : false }
-  )
-})
-
-app.get('/brad', function (req, res) {
-  res.render('index',
-  { title : 'BB',
-    alert : true }
-  )
-})
-
-
-app.listen(3000)
-
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
